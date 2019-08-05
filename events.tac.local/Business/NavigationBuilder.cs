34cae66232d371c87f5929157a5c6d29873e8ad4
Mukesh Bhojwani <bhojwanimukesh@gmail.com>
2019-08-05 11:28:27 +0530

    Added the items to the new repository

---
+++ events.tac.local/Business/NavigationBuilder.cs
@@ -0,0 +1,64 @@
+﻿using events.tac.local.Models;
+using System;
+using TAC.Sitecore.Abstractions.Interfaces;
+using TAC.Sitecore.Abstractions.SitecoreImplementation;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace events.tac.local.Business
+{
+    public class NavigationBuilder
+    {
+        private readonly IRenderingContext _context;
+
+        public NavigationBuilder() : this(SitecoreRenderingContext.Create()) { }
+
+        public NavigationBuilder(IRenderingContext context)
+        {
+            _context = context;
+        }
+
+        public NavigationMenuItem Build()
+        {
+            var root = GetRoot(_context?.DatasourceOrContextItem);
+
+            return new NavigationMenuItem
+            (
+                title    : root?.DisplayName,
+                url      : root?.Url,
+                children : root != null && _context?.ContextItem != null ? Build(root, _context.ContextItem) : null
+            );
+        }
+
+        private IEnumerable<NavigationMenuItem> Build(IItem node, IItem current)
+        {
+            return node
+                .GetChildren()
+                .Select(i => new NavigationMenuItem
+                (
+                    title: i.DisplayName,
+                    url: i.Url,
+                    children: i.IsAncestorOrSelf(current) ? Build(i, current) : null
+                ));
+        }
+
+        private IItem GetRoot(IItem item)
+        {
+            if (item != null)
+            {
+                //Get "Events" parent item
+                if (item.Name != "Events")
+                {
+                    return GetRoot(item.Parent);
+                }
+                else
+                {
+                    return item;
+                }
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ events.tac.local/Default.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="events.tac.local.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Default Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p>Default Page</p>
+        </div>
+    </form>
+</body>
+</html>
